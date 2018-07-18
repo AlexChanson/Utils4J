3d71fa03e397b12e89a6d40bc6a231358920bab7
Alexandre Chanson <a.chanson.tours@gmail.com>
2018-07-18 15:42:30 +0200

    added some basic stuff

---
+++ src/com/alexsxode/utilities/collection/Pair.java
@@ -0,0 +1,24 @@
+package com.alexsxode.utilities.collection;
+
+public class Pair<L, R> {
+    public final L left;
+    public final R right;
+
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public L getLeft() {
+        return left;
+    }
+
+    public R getRight() {
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return left.toString();
+    }
+}
