0b14afe34c8c7248da3f013b6a308b32f16da108
Alexandre Chanson <a.chanson.tours@gmail.com>
2019-01-31 16:47:37 +0100

    added equals

--- src/com/alexsxode/utilities/collection/Pair.java
+++ src/com/alexsxode/utilities/collection/Pair.java
@@ -1,5 +1,7 @@
 package com.alexsxode.utilities.collection;
 
+import java.util.Objects;
+
 public class Pair<L, R> {
     public final L left;
     public final R right;
@@ -15,6 +17,30 @@ public class Pair<L, R> {
 
     public R getRight() {
         return right;
+    }
+
+    public L getA(){
+        return left;
+    }
+
+    public R getB(){
+        return right;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(getA(), pair.getA()) &&
+                Objects.equals(getB(), pair.getB());
     }
 
     @Override
